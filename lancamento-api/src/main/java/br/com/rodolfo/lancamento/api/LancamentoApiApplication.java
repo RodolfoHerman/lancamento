a31a6b075bbdde99108ebfdabd2d547a9998fdc8
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2020-01-17 21:04:45 -0300

    Backend - Commit inicial do projeto

---
+++ lancamento-api/src/main/java/br/com/rodolfo/lancamento/api/LancamentoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodolfo.lancamento.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentoApiApplication.class, args);
+	}
+
+}
